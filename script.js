28136c3097c38d14638568ac9a4dda36f3c094ad
Aarya <amathrej@asu.edu>
2022-05-15 18:02:23 -0700

    Add focus and blur event listeners to input tag

--- script.js
+++ script.js
@@ -41,3 +41,15 @@ button.addEventListener('click', function (e) {
 button.textContent = 'Click me';
 
 container.appendChild(button);
+
+let help = document.querySelector("#help");
+let fields = document.querySelectorAll("input");
+for (let field of Array.from(fields)) {
+  field.addEventListener("focus", event => {
+    let text = event.target.getAttribute("data-help");
+    help.textContent = text;
+  });
+  field.addEventListener("blur", event => {
+    help.textContent = "";
+  });
+}
